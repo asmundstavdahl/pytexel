d3eb347c3c924de2155ffbacc48f036a1ac8eb08
Åsmund Stavdahl <asmund.stavdahl@ntnu.no>
2021-01-27 07:54:15 +0100

    improve demo-pygame-line.py
    - limit to 60 FPS
    - better terminal treatment

--- demo-pygame-line.py
+++ demo-pygame-line.py
@@ -3,6 +3,7 @@
 import pygame
 from pygame.locals import *
 import numpy as np
+import time
 from PIL import Image
 from texelator import Texelator
 
@@ -12,12 +13,32 @@ pygame.init()
 CFg: pygame.Color = pygame.Color("black")
 CBg: pygame.Color = pygame.Color("white")
 displaySize = (500, 500)
+texplaySize = (20, 10)
+frameTime = (1/60)
 
-print("\n"*90)
+
+def waitForNextFrame(tPrev) -> (float, float):
+    tNow = time.time()
+    dt = tNow - tPrev
+    if dt < frameTime:
+        time.sleep(frameTime - dt)
+        tNow = time.time()
+        dt = tNow - tPrev
+    tPrev = tNow
+    print(f"FPS={(1/dt) - (1/dt) % 0.1}")
+    return (dt, tPrev)
+
+
+print("\x1b[2J")
+print("\x1b[H")
+print("\n" * texplaySize[1])
 
 display = pygame.surface.Surface(displaySize)
 
+tPrev = time.time() - frameTime
 for i in range(450):
+    (dt, tPrev) = waitForNextFrame(tPrev)
+
     display.fill(CBg)
 
     pygame.draw.line(display, CFg, (50, 50 + i),
@@ -27,6 +48,8 @@ for i in range(450):
     image = Image.frombuffer(
         "RGBX", displaySize, displayBuffer, "raw", "RGBX", 0, 1)
 
-    output = texelator.render(image, 60, 30)
-    print(output)
+    output = texelator.render(image, texplaySize[0], texplaySize[1])
     print("\x1b[H")
+    print(output)
+
+print()
