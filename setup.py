f7806a22ce79d73c4cd673c50be95ecafc0a0931
Åsmund Stavdahl <asmundstavdahl@users.noreply.github.com>
2022-08-05 16:41:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from pytexel import __version__
+
+setup(
+    name='pytexel',
+    version=__version__,
+
+    url='https://github.com/asmundstavdahl/pytexel',
+    author='Åsmund Stavdahl',
+    author_email='asmund.stavdahl@gmail.com',
+
+    py_modules=['pytexel'],
+)
